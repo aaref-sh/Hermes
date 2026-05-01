9a0a68dceb081e3eab5b967fa305527911fb1acc
Muhammad Aaref <40777651+aaref-sh@users.noreply.github.com>
2026-05-01 14:29:02 +0300

    update product fix

--- HStore.Application/DTOs/UpdateProductDto.cs
+++ HStore.Application/DTOs/UpdateProductDto.cs
@@ -1,9 +1,11 @@
-﻿﻿namespace HStore.Application.DTOs;
+﻿using HStore.Domain.Classes;
+
+namespace HStore.Application.DTOs;
 
 public class UpdateProductDto
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public LocalizedProperty Name { get; set; }
+    public LocalizedProperty Description { get; set; }
     public decimal Price { get; set; }
     public string ImageUrl { get; set; }
     public List<int>? CategoryIds { get; set; }
